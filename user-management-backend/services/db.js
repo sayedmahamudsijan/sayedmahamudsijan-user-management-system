641b5abba51be2cebadc0cfa4ba3150260963dec
sayedmahamudsijan <112859381+sayedmahamudsijan@users.noreply.github.com>
2025-06-05 18:46:33 +0600

    Update db.js

--- user-management-backend/services/db.js
+++ user-management-backend/services/db.js
@@ -1,10 +1,9 @@
-const mysql = require('mysql2/promise');
+const { Pool } = require('pg');
+require('dotenv').config();
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+const pool = new Pool({
+  connectionString: process.env.POSTGRES_URL,
+  ssl: { rejectUnauthorized: false }
 });
 
 module.exports = pool;
