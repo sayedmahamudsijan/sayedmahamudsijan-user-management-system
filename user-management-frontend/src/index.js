3508c2a91b0e466f2b5ac0ec132f5bc0df295dd0
sayedmahamudsijan <112859381+sayedmahamudsijan@users.noreply.github.com>
2025-06-05 19:14:16 +0600

    Update index.js

--- user-management-frontend/src/index.js
+++ user-management-frontend/src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
